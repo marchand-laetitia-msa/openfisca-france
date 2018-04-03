1336944f121573aab34dccaa1da901a03f30816d
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-03 18:37:07 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France-test',
-    version = '21.0.0ci',
+    version = '21.0.2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
